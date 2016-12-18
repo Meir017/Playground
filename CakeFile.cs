87d8de09b5c1baef59e2f44ff0a87995fe29fd6d
Meir017 <meirblachman@gmail.com>
2016-12-18 22:00:19 +0200

    Update CakeFile.cs

--- CakeFile.cs
+++ CakeFile.cs
@@ -1,47 +1,26 @@
-public abstract class CakeFile
+using System;
+using Cake.Core;
+
+namespace ConsoleApp1
+{
+    public abstract class CakeFile
     {
-        protected ICakeContext Context { get; }
+        protected ICakeContext Context => throw new NotSupportedException();
 
         public abstract void Execute();
 
-        protected CakeTaskBuilder<ActionTask> Task(string name)
-        {
-            throw new NotSupportedException();
-        }
+        protected CakeTaskBuilder<ActionTask> Task(string name) => throw new NotSupportedException();
 
-        protected void TaskSetup(Action<ICakeContext, ActionTask> action)
-        {
-            throw new NotSupportedException();
-        }
-        protected void TaskTeardown(Action<ICakeContext, ActionTask> action)
-        {
-            throw new NotSupportedException();
-        }
+        protected void TaskSetup(Action<ICakeContext, ActionTask> action) => throw new NotSupportedException();
+        protected void TaskTeardown(Action<ICakeContext, ActionTask> action) => throw new NotSupportedException();
 
-        protected void Setup(Action<ICakeContext> action)
-        {
-            throw new NotSupportedException();
-        }
-        protected void Teardown(Action<ICakeContext> action)
-        {
-            throw new NotSupportedException();
-        }
+        protected void Setup(Action<ICakeContext> action) => throw new NotSupportedException();
+        protected void Teardown(Action<ICakeContext> action) => throw new NotSupportedException();
 
-        protected void RunTarget(string name)
-        {
-            throw new NotSupportedException();
-        }
+        protected void RunTarget(string name) => throw new NotSupportedException();
 
-        protected void Addin(string addin)
-        {
-            throw new NotSupportedException();
-        }
-        protected void Tool(string tool)
-        {
-            throw new NotSupportedException();
-        }
-        protected void Load(string file)
-        {
-            throw new NotSupportedException();
-        }
+        protected void Addin(string addin) => throw new NotSupportedException();
+        protected void Tool(string tool) => throw new NotSupportedException();
+        protected void Load(string file) => throw new NotSupportedException();
     }
+}
