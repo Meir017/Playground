d52a59f0c6b938b1d832ba794829d19eb3ec2748
Meir Blachman <meirblachman@gmail.com>
2018-06-10 22:06:35 +0300

    added flaky tests demo

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Random ideas for random projects
 
-## Cake Ideas
+## cake-ideas
 
 before omnisharp has intellisense for cake there wasn't an easy way to have intellisense for cake scripts, this was an attempt to generate intellisense for cake scripts.
+
+## mstest-ideas
+
+### FlakyTestMethod
+
+Can be used when tests are flaky (selenium tests)
---
+++ mstest-ideas/FlakyTestMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Playground.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class FlakyTestMethodAttribute : TestMethodAttribute
+    {
+        public int RetryCount { get; set; }
+
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            TestResult result = testMethod.Invoke(new object[0]);
+            var output = new StringBuilder();
+            for (int i = 0; i < RetryCount; i++)
+            {
+                if (result.Outcome == UnitTestOutcome.Passed) break;
+                output.AppendLine($"Run #{i + 1} completed with status {result.Outcome}. exception: {result.TestFailureException}");
+                result = testMethod.Invoke(new object[0]);
+            }
+            if (output.Length > 0)
+            {
+                result.LogOutput += "[Failures]" + Environment.NewLine + output;
+            }
+            return new[] { result };
+        }
+    }
+
+    [TestClass]
+    public class TestClass
+    {
+        [FlakyTestMethod(RetryCount = 6)]
+        public void FlakyTest()
+        {
+            Assert.IsTrue(new Random().NextDouble() > 0.8);
+        }
+    }
+}
